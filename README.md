151289ba4289e4773a4d3bb720b3316ae90179aa
Helvennn <enixgregory@gmail.com>
2024-12-09 11:56:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atom Cipher
+An innovative password encryption tool that transforms passwords into encrypted sequences using the atomic numbers of chemical elements from the periodic table.
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// MODULE
+// import { useState } from 'react'
+
+// COMPONENTS
+
+
+// CSS
+import './css/App.css'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default App
